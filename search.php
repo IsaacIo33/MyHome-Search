c24814c426c78be6405244713a5934f4a57a31d1
IsaacIo33 <isaacio@vivaldi.net>
2026-08-08 11:52:32 -0400

    Add CSS

--- search.php
+++ search.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Searching...</title>
+    <style>
+        body {
+            font-family:sans-serif;
+            cursor:default;
+            user-select:none;
+        }
+    </style>
+</head>
+<body>
+
 <?php
 
 $q = $_GET["q"];
@@ -7,3 +23,8 @@ echo "Please wait...";
 echo "<script>window.location.href=`search?q=$q`;</script>";
 
 ?>
+
+
+</body>
+</html>
+
