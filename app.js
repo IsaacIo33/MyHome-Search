6f6d3bc85ba667d3aca87a3459699ce77681684b
IsaacIo33 <isaacio@vivaldi.net>
2026-08-08 11:49:57 -0400

    Added Clock JS code

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 let yearEl = document.getElementById("year");
+let hoursEl = document.getElementById("hours");
+let minutesEl = document.getElementById("minutes");
+let secondsEl = document.getElementById("seconds");
+let ampmEl = document.getElementById("ampm");
+let date = new Date();
 
-yearEl.innerText = new Date().getFullYear();
+yearEl.innerText = date.getFullYear();
+
+
+function update(){
+    date = new Date();
+    let hours = date.getHours();
+    let ampm = "AM";
+
+    if (hours >= 12){
+        ampm = "PM";
+        hours -= 12;
+    }
+
+    hoursEl.innerText = hours;
+    minutesEl.innerText = date.getMinutes();
+    secondsEl.innerText = date.getSeconds();
+    ampmEl.innerText = ampm;
+}
+
+setInterval(update, 200);
